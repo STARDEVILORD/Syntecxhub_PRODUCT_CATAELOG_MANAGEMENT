e8aba234b6b01579fa5eb66a854d55254b2dd9ec
SHRIRAM <shriramuday12345610@gmail.com>
2026-06-16 21:10:21 +0530

    authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-// @desc    Get Mock JWT for testing
-// @route   GET /api/auth/token
 exports.getTestToken = (req, res) => {
     // Generate a token valid for 1 hour
     const token = jwt.sign({ id: 'admin123', role: 'admin' }, process.env.JWT_SECRET, {
